ae87d959ae19a807b988f10e4f7e4bbf84969224
Yann Eduardo de Souza Silva <yannsouzasilva53@gmail.com>
2024-02-20 18:50:22 -0300

    tp1

---
+++ tp1_ed1/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "grafo.h"
+
+
+int main() {
+    int num_cidades;
+
+    scanf("%d", &num_cidades);
+
+    GrafoPonderado *grafo = alocarGrafo(num_cidades);
+    leGrafo(grafo);
+
+    int *caminho = (int*)malloc(grafo->numero_de_cidades * sizeof(int));
+    int *melhor_caminho = (int*) malloc(grafo->numero_de_cidades  * sizeof(int));
+    int *cidades_visitadas = (int*) malloc(grafo->numero_de_cidades * sizeof(int));
+    int menor_distancia = __INT_MAX__;
+    
+    encontraCaminho(0,0,grafo, caminho,cidades_visitadas,&menor_distancia,melhor_caminho);
+
+    imprimeCaminho(num_cidades, melhor_caminho,menor_distancia);
+    
+    desalocaGrafo(&grafo,&caminho,&melhor_caminho,&cidades_visitadas);
+
+    return 0;
+}
+
